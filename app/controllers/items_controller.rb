ca4619b9b868673155fe2a041a007619fbbf9786
Dang Nguyen <minhdang.net@gmail.com>
2013-03-19 16:16:56 +0700

    update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,30 @@
 class ItemsController < ApplicationController
   java_import "java_code.Warehouse.WarehouseInfo"
   java_import "java_code.Warehouse.Item"
+  java_import "java_code.Warehouse.APIMessageListHolder"
   def index
-    params[:warehouse_id]
-    item1 = Item.new
-    item1.itemCode = 1
-    item1.description = "abc"
-    @items = []
-    @items<<item1
+    
+    @warehouseClient = WarehouseClient.new(WareHouse::Application.config.api_host, WareHouse::Application.config.api_port)
+    apiMessageListHolder = APIMessageListHolder.new
+    warehouse_id = params[:warehouse_id].to_i
+    @items =  @warehouseClient.getItemList(1, cookies[:remember_token], apiMessageListHolder)
 
+  end
+
+  def search
+    w1 = WarehouseInfo.new
+    w1.id = 1
+    w1.name = "abc"
+    w2 = WarehouseInfo.new
+    w2.id = 2
+    w2.name = "abc 2"
+    @warehouses = []
+    @warehouses<<w1
+    @warehouses<<w2
+
+
+  end
+
+  def search_results
   end
 end
--- java_code/WarehouseClient.java
+++ java_code/WarehouseClient.java
@@ -158,4 +158,42 @@ public class WarehouseClient {
 		}
 		return result;
 	}
+	void updateUser (java_code.Warehouse.User user, String token, java_code.Warehouse.APIMessageListHolder apiMessageList) {
+
+	}
+  java_code.Warehouse.Role getRole (short role_id, String token, java_code.Warehouse.APIMessageListHolder apiMessageList) {
+  	return null;	
+  }
+
+  java_code.Warehouse.Item[] getItemList(short warehouse_id, String token, java_code.Warehouse.APIMessageListHolder apiMessageList) {
+  	java_code.Warehouse.Item[] result = null;
+		try{
+			String[] params = new String[4];
+			params[0] = "-ORBInitialPort";
+			params[1] = port.toString();
+			params[2] = "-ORBInitialHost";
+			params[3] = host;
+			//	create	and	initialize	the	ORB
+			ORB	orb	=	ORB.init(params,	null);
+			//	get	the	root	naming	context
+			org.omg.CORBA.Object	objRef	=
+			orb.resolve_initial_references("NameService");
+			//	Use	NamingContextExt	instead	of	NamingContext.	This	is
+			//	part	of	the	Interoperable	naming	Service.
+			NamingContextExt	ncRef	=	NamingContextExtHelper.narrow(objRef);
+			//	resolve	the	Object	Reference	in	Naming
+			String	name	=	"WHM";
+			warehouseImpl	=	WHMHelper.narrow(ncRef.resolve_str(name));
+			System.out.println("Obtained	a	handle	on	server	object:	"	+	warehouseImpl);
+
+			result = warehouseImpl.getItemList(warehouse_id, token, apiMessageList);
+
+		}	catch	(Exception	e)	{
+			System.out.println("ERROR	:	"	+	e)	;
+			e.printStackTrace(System.out);
+		}
+		return result;			
+  }
+
+
 }
